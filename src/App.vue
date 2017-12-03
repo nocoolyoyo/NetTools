1454fb2c4dbb6c4e0c3033cd40905124e780c158
Leon Hyoung <574857444@qq.com>
2017-12-03 23:50:53 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <el-container id="app">
+    <el-header>
+      <div class="logo h1">Http Tools</div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "styles/variables";
+
+  html,body {
+    overflow: hidden;
+    height: 100%;
+  }
+  #app {
+  overflow: hidden;
+  height: 100%;
+    .el-header {
+      z-index: 1;
+      background-color: $c-milka;
+      box-shadow: $box-shadow-main;
+      .logo {
+        float: left;
+        height:inherit;
+        display: flex;
+        align-items: center;
+        font-size: 30px;
+        font-weight: bold;
+      }
+    }
+    .el-aside {
+      box-shadow: $box-shadow-main;
+      background-color: white;
+      .el-menu {
+        border-right: none;
+      }
+
+    }
+  }
+</style>
